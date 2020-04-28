97e7f6c3e79067b660daebbc22ac2d9b60a116d7
karim nour eldin <36403473+karimnour@users.noreply.github.com>
2020-04-28 10:40:26 +0200

    Add files via upload

---
+++ ListPermutations.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+vector <string> vec;
+
+void  RecPermute(string soFar, string rest)
+{
+    if (rest == "")
+    {    // No more characters
+         // cout << "soFar"<< endl;
+         // cout << soFar<< endl;
+
+    }
+
+    for (int i = 0; i < rest.length(); ++i)
+    {
+        string next = soFar + rest[i];// Glue next char
+        //cout <<"next"<<endl;
+        //cout <<next<<endl;
+        string remaining = rest.substr(0, i)+ rest.substr(i+1);
+        //cout<<"remaining"<<endl;
+        //cout<<remaining<<endl;
+        if (remaining=="")
+        {
+
+            vec.push_back(next);
+
+            //cout <<vec.size()<<endl;
+            //cout <<vec[t]<<endl;
+
+           RecPermute(next, remaining);
+        }
+        else
+        {
+             RecPermute(next, remaining);
+
+        }
+
+
+
+    }
+
+
+}
+/*--------------------------------------------------------------*/
+// "wrapper" function
+void ListPermutations(string s ) {
+    RecPermute("", s );
+}
+/*-------------------------------------------------------------------*/
+
+int main()
+{
+    string input ;
+    cout <<"enter your string "<<endl;
+    cin >>input;
+    ListPermutations(input);
+
+    /*
+    cout << "size = " << vec.size() << endl;
+
+    for(auto i = 0; i < vec.size(); ++i)
+    {
+        cout << "vec[" << i << "] = " << vec.at(i)<< endl;
+    }
+    */
+
+    sort(vec.begin(), vec.end());
+
+    auto it = unique(vec.begin(), vec.end());
+    vec.erase(it, vec.end());
+
+    for(auto i = 0; i < vec.size(); ++i)
+    {
+        cout << "vec[" << i << "] = " << vec.at(i)<< endl;
+    }
+
+    return 0;
+}
